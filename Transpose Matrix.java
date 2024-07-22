f51cc37532ed78f5c289ff0857c69923269a1e6d
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-22 13:30:24 +0530

    Create Transpose Matrix.java

---
+++ Transpose Matrix.java
@@ -0,0 +1,25 @@
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("Transpose of matrix:");
+        for (int[] row : transpose) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
