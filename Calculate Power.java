7535a08a209cbf0656e7226fe9a5dd54ee2f2350
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-22 13:33:46 +0530

    Create Calculate Power.java

---
+++ Calculate Power.java
@@ -0,0 +1,13 @@
+public class CalculatePower {
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 3;
+        long result = 1;
+
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
