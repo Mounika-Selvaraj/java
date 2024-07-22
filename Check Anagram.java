2f9ba19ff5cb7b4b176f98e058be215ecb25ab73
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-22 13:33:13 +0530

    Create Check Anagram.java

---
+++ Check Anagram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class CheckAnagram {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (isAnagram(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+
+        char[] array1 = str1.toCharArray();
+        char[] array2 = str2.toCharArray();
+
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+
+        return Arrays.equals(array1, array2);
+    }
+}
