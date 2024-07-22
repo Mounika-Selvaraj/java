bd1bebb544b36c719b02576302812bac2a1adc9e
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-22 13:32:48 +0530

    Create Integer to String.java

---
+++ Integer to String.java
@@ -0,0 +1,7 @@
+public class IntegerToString {
+    public static void main(String[] args) {
+        int number = 12345;
+        String str = Integer.toString(number);
+        System.out.println("Converted string: " + str);
+    }
+}
